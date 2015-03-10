215b1a3abc34de927b9b7d3798a9eda2d0ebe750
Jake Marwil <rjm02006@gmail.com>
2015-03-10 10:40:47 -0600

    create Tweet class with consumer keys and access tokens

---
+++ app/models/tweet.rb
@@ -0,0 +1,9 @@
+class Tweet
+
+  client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "JBUUEREJoCKyfmQNgFUuRtaXq"
+  config.consumer_secret     = "HHLezh2Dg6ivvkESSpqlcZCOejFPP86iXwQ0d42RMcLHgJcuHg"
+  config.access_token        = "2986038090-hk87mFbtzhsNC36n9rI3fkNO1jkSXE4TCpSk9YQ"
+  config.access_token_secret = "7hQSnNwgmBVrabvuqkpJCQfwgevpMC1vro66CoEZwY22v"
+
+end
